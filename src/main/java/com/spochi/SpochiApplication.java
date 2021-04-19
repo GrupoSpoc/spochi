1d42b765bb8c3815253cae57b961eaa8c8ac92d0
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-19 13:09:14 -0300

    creo proyecto y directorios

---
+++ src/main/java/com/spochi/SpochiApplication.java
@@ -0,0 +1,13 @@
+package com.spochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpochiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spochi/SpochiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpochiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
