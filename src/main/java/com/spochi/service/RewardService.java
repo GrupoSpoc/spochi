c63c624f4b5061b709625db35175d432ff3aa3cc
gaston <gaston.92@hotmail.com.ar>
2021-04-24 18:27:41 -0300

    Primer commit, se agregan las clases de los services y sus repositorios comentados para enlazar mas adelante

---
+++ src/main/java/com/spochi/service/RewardService.java
@@ -0,0 +1,11 @@
+package com.spochi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RewardService {
+
+    //@Autowired
+   // RewardRepository repository;
+}
