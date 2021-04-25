37c52f6417e0c1a5c0ca63e638e581c1b52c4793
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-25 19:24:29 -0300

    SPOCAN-41 SPOCAN-44 agrego repositorios a los services

--- src/main/java/com/spochi/service/UserService.java
+++ src/main/java/com/spochi/service/UserService.java
@@ -1,11 +1,11 @@
 package com.spochi.service;
 
+import com.spochi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    //@Autowired
-    //UserRepository repository;
+    @Autowired
+    UserRepository repository;
 }
