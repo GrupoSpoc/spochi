7ed5e1349a535c77ca6288a55080a763ab22f52f
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-06 22:53:26 -0300

    SPOCAN-47 borro metodos sin uso en JwtUtil

--- src/main/java/com/spochi/service/authenticate/JwtUtil.java
+++ src/main/java/com/spochi/service/authenticate/JwtUtil.java
@@ -1,6 +1,6 @@
 package com.spochi.service.authenticate;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
@@ -8,34 +8,12 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import io.jsonwebtoken.Jwts;
 
 
 @Service
 public class JwtUtil {
 
     private final String secret = "desafio-final";
-
-    public String extractGoogleId(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
 
     public String generateToken(String userId) {
         Map<String, Object> claims = new HashMap<>();
@@ -46,10 +24,6 @@ public class JwtUtil {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
-    }
-
-    public boolean isTokenValid(String token) {
-        return !isTokenExpired(token);
     }
 }
 
