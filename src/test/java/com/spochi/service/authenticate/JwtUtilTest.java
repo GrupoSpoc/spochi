8c98e39a10f07d5b42bd4e1859b29b408b0110ed
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-07 19:45:50 -0300

    agrego perfil en test

--- src/test/java/com/spochi/service/authenticate/JwtUtilTest.java
+++ src/test/java/com/spochi/service/authenticate/JwtUtilTest.java
@@ -7,8 +7,8 @@ import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("disable-firebase")
 class JwtUtilTest {
-
     @Test
     void generateToken() {
         assertNotNull(new JwtUtilForTest().generateToken("token"));
