7f6033a2004b983b6df17908dec9132b8079a50c
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-08 00:16:00 -0300

    SPOCAN-59 agrego funcionalidades a JwtUtil

--- src/main/java/com/spochi/service/authenticate/JwtUtil.java
+++ src/main/java/com/spochi/service/authenticate/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.spochi.service.authenticate;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,19 +17,43 @@ public class JwtUtil {
     @Value("${jwt.secret.key}")
     private String secretKey;
 
-    public String generateToken(String userId) {
+    public String generateToken(String uid) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userId);
+        return createToken(claims, uid);
+    }
+
+    public boolean isTokenValid(String token) {
+        return !isTokenExpired(token);
+    }
+
+    public String extractUid(String token) {
+        return extractAllClaims(token).getSubject();
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
+                .setExpiration(new Date(currentMillis() + TimeUnit.HOURS.toMillis(3)))
                 .signWith(SignatureAlgorithm.HS256, getSecretKey()).compact();
+    }
+
+    private Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(getSecretKey()).parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date(currentMillis()));
     }
 
     protected String getSecretKey() {
         return secretKey;
+    }
+
+    protected long currentMillis() {
+        return System.currentTimeMillis();
     }
 }
 
