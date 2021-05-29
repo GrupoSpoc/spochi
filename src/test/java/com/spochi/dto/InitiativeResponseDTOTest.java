dda9ecccdd7e35fae5e71f0cd4f21333e33828f0
lPretti <lprettimolina@gmail.com>
2021-05-28 22:19:16 -0300

    SPOCAN-108

--- src/test/java/com/spochi/dto/InitiativeResponseDTOTest.java
+++ src/test/java/com/spochi/dto/InitiativeResponseDTOTest.java
@@ -12,29 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class InitiativeResponseDTOTest {
 
     @Test
-    void equalsWorks() {
+    void initiativeResponseDtoOk() {
         final String nickname = "author";
         final LocalDateTime date = LocalDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC"));
-        final String descprition = "description";
+        final String description = "description";
         final String image = "image";
         final String user = "user_";
 
+        final Initiative initiative = new Initiative();
+        initiative.setImage(image);
+        initiative.setNickname(nickname);
+        initiative.setDate(date);
+        initiative.setStatusId(2);
+        initiative.setUserId(user);
 
-        final Initiative.InitiativeBuilder builder = Initiative.builder();
-        builder.nickname(nickname);
-        builder.date(date);
-        builder.description(descprition);
-        builder.statusId(2);
-        builder.image(image);
-        builder.userId(user);
+        final InitiativeResponseDTO dto = initiative.toDTO(user);
 
-        InitiativeResponseDTO dto = builder.build().toDTO(user);
-
-        assertEquals(nickname,dto.getNickname());
-        assertEquals(date.toString(),dto.getDate());
-        assertEquals(descprition,dto.getDescription());
-        assertEquals(image,dto.getImage());
-        assertEquals(2,dto.getStatus_id());
+        assertEquals(initiative.getNickname(),dto.getNickname());
+        assertEquals(initiative.getDate().toString(),dto.getDate());
+        assertEquals(initiative.getDescription(),dto.getDescription());
+        assertEquals(initiative.getImage(),dto.getImage());
+        assertEquals(initiative.getStatusId(),dto.getStatus_id());
         assertTrue(dto.isFrom_current_user());
     }
 
