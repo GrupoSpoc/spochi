60a67f75edb61cfc43915781d7bf8d7777633249
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-06-07 12:40:11 -0300

    SPOCAN-95 arreglos para peticiones pre-flight de admin, cors y client id de React

--- src/main/java/com/spochi/config/WebConfig.java
+++ src/main/java/com/spochi/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.spochi.controller.handler.UidHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,4 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(uidHandler);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+
 }
